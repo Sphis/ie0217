789c4a4827ea991a98c96cb96f5cc7d6dd9077db
KevinCamposC <KEVIN.CAMPOSCASTRO@ucr.ac.cr>
2024-03-31 12:30:58 -0600

    Se agregan los archivos restantes para el previo 4 ya probados y verificados.

---
+++ Previos/Previo4/friend_class.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class ClassB;
+
+class ClassA {
+    private:
+        int numA;
+
+        // friend class declaration
+        friend class ClassB;
+
+    public:
+        // constructor to initialize numA to 12
+        ClassA(): numA (12) {}
+};
+
+class ClassB {
+    private:
+        int numB;
+
+    public:
+        // constructor to initialize numb to 1
+        ClassB(): numB (1) {}
+
+    // member function to add numA
+    // from ClassA and numB from ClassB
+    int add() {
+        ClassA objectA;
+        return objectA.numA + numB;
+    }
+};
+
+int main() {
+    ClassB objectB;
+    cout << "Sum: " << objectB.add();
+    return 0;
+}
